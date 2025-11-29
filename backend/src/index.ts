e51d57f174757824ebea5719df833841b5b1bbc9
Waseem Akram <waseem.akram@syven.com>
2025-11-29 20:13:29 +0530

    fix:cors issue fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gather-mernstac-frontend.onrender.com",
     credentials: true,
   })
 );
